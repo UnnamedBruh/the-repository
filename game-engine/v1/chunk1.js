5ac7b9e0b89a9ce6cda6ca167ace8dc0817fca47
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-10-20 09:03:01 -0700

    Update chunk1.js

--- game-engine/v1/chunk1.js
+++ game-engine/v1/chunk1.js
@@ -154,4 +154,247 @@ const UnnamedEngine = (function() {
 			return new Vector2(Math.cbrt(this.#Xp), Math.cbrt(this.#Yp))
 		}
 	}
+	class IntVector2 extends Vector2 {
+		constructor(x = 0, y = 0) {
+			super(x, y)
+			this.#Xp = Math.floor(this.#x)
+			this.#Yp = Math.floor(this.#y)
+		}
+		set x(val) {
+			this.#Xp = Math.floor(Number(val))
+			return this.#Xp
+		}
+		set y(val) {
+			this.#Yp = Math.floor(Number(val))
+			return this.#Yp
+		}
+		exp() {
+			return new IntVector2(Math.exp(this.#Xp), Math.exp(this.#Yp))
+		}
+		cbrt() {
+			return new IntVector2(Math.cbrt(this.#Xp), Math.cbrt(this.#Yp))
+		}
+		sqrt() {
+			return new IntVector2(Math.sqrt(this.#Xp), Math.sqrt(this.#Yp))
+		}
+		set magnitude(val) {
+			if (arr.has((typeof val)[0])) {
+				const th = Number(val)
+				if (isNaN(th)) {
+					throw new Error(`The value setting the magnitude of a Vector2 must be a number or a numeric string (this value is '${val}')`)
+				}
+				switch (th) {
+					case 1:
+						const u = this.unit
+						this.#Xp = u.x
+						this.#Yp = u.y
+						return u
+					case 0:
+						this.#Xp = 0
+						this.#Yp = 0
+						return new IntVector2(0, 0)
+					default:
+						const m = this.magnitude
+						this.#Xp = (this.#Xp / m) * th
+						this.#Yp = (this.#Yp / m) * th
+						return new IntVector2(this.#Xp, this.#Yp)
+				}
+			} else {
+				throw new Error(`The value setting the magnitude of a Vector2 must be a number or a numeric string (this value is '${val}')`)
+			}
+		}
+		add(vector, num2) {
+			if (vector instanceof IntVector2) {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x += vector.x
+				vect.y += vector.y
+				return vect
+			} else if (num2 === undefined && (typeof vector)[0] === "n") {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x += vector
+				vect.y += vector
+				return vect
+			} else if ((typeof vector)[0] === "n" && (typeof num2)[0] === "n") {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x += vector
+				vect.y += num2
+				return vect
+			} else {
+				throw new TypeError("The 'add' function part of the IntVector2 class must be provided with either:\n1. Another Vector2.\n2. A number.\n3. Two numbers.")
+			}
+		}
+		sub(vector, num2) {
+			if (vector instanceof IntVector2) {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x -= vector.x
+				vect.y -= vector.y
+				return vect
+			} else if (num2 === undefined && (typeof vector)[0] === "n") {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x -= vector
+				vect.y -= vector
+				return vect
+			} else if ((typeof vector)[0] === "n" && (typeof num2)[0] === "n") {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x -= vector
+				vect.y -= num2
+				return vect
+			} else {
+				throw new TypeError("The 'sub' function part of the IntVector2 class must be provided with either:\n1. Another Vector2.\n2. A number.\n3. Two numbers.")
+			}
+		}
+		mul(vector, num2) {
+			if (vector instanceof IntVector2) {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x *= vector.x
+				vect.y *= vector.y
+				return vect
+			} else if (num2 === undefined && (typeof vector)[0] === "n") {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x *= vector
+				vect.y *= vector
+				return vect
+			} else if ((typeof vector)[0] === "n" && (typeof num2)[0] === "n") {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x *= vector
+				vect.y *= num2
+				return vect
+			} else {
+				throw new TypeError("The 'mul' function part of the IntVector2 class must be provided with either:\n1. Another Vector2.\n2. A number.\n3. Two numbers.")
+			}
+		}
+		div(vector, num2) {
+			if (vector instanceof IntVector2) {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x /= vector.x
+				vect.y /= vector.y
+				return vect
+			} else if (num2 === undefined && (typeof vector)[0] === "n") {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x /= vector
+				vect.y /= vector
+				return vect
+			} else if ((typeof vector)[0] === "n" && (typeof num2)[0] === "n") {
+				const vect = new IntVector2(this.#Xp, this.#Yp)
+				vect.x /= vector
+				vect.y /= num2
+				return vect
+			} else {
+				throw new TypeError("The 'div' function part of the IntVector2 class must be provided with either:\n1. Another Vector2.\n2. A number.\n3. Two numbers.")
+			}
+		}
+	}
+	class Color3 {
+		#r = 0;
+		#g = 0;
+		#b = 0;
+		constructor(r = 0, g, b) {
+			r = Number(r)
+			if (!isNaN(r) && g === undefined && b === undefined) {
+				r = Math.floor(r)
+				this.#r = r
+				this.#g = r
+				this.#b = r
+			} else {
+				g = Number(g)
+				b = Number(b)
+				if (!isNaN(r) && !isNaN(g) && !isNaN(b)) {
+					this.#r = Math.floor(r)
+					this.#g = Math.floor(g)
+					this.#b = Math.floor(b)
+				} else {
+					throw new TypeError("The Color3 class must be provided with: 1. One number or numerical string.\n2. Three values of any number or numerical string.")
+				}
+			}
+		}
+		get r() {
+			return this.#r
+		}
+		get g() {
+			return this.#g
+		}
+		get b() {
+			return this.#b
+		}
+		get R() {
+			return this.#r
+		}
+		get G() {
+			return this.#g
+		}
+		get B() {
+			return this.#b
+		}
+		set r(val) {
+			val = Number(val)
+			if (!isNaN(val)) {
+				this.#r = Math.floor(val)
+			} else {
+				throw new TypeError("The provided value must be a number, or a numerical string (setting the 'r' component to '" + val + "' has been halted)")
+			}
+			return this.#r
+		}
+		set g(val) {
+			val = Number(val)
+			if (!isNaN(val)) {
+				this.#g = Math.floor(val)
+			} else {
+				throw new TypeError("The provided value must be a number, or a numerical string (setting the 'g' component to '" + val + "' has been halted)")
+			}
+			return this.#g
+		}
+		set b(val) {
+			val = Number(val)
+			if (!isNaN(val)) {
+				this.#b = Math.floor(val)
+			} else {
+				throw new TypeError("The provided value must be a number, or a numerical string (setting the 'b' component to '" + val + "' has been halted)")
+			}
+			return this.#b
+		}
+		static black() {
+			return new Color3(0)
+		}
+		static white() {
+			return new Color3(255)
+		}
+		static red() {
+			return new Color3(255, 0, 0)
+		}
+		static yellow() {
+			return new Color3(255, 255, 0)
+		}
+		static green() {
+			return new Color3(0, 255, 0)
+		}
+		static aqua() {
+			return new Color3(0, 255, 255)
+		}
+		static blue() {
+			return new Color3(0, 0, 255)
+		}
+		static purple() {
+			return new Color3(128, 0, 255)
+		}
+		static pink() {
+			return new Color3(255, 0, 255)
+		}
+	}
+	class SquareDisplay {
+		#pos = null;
+		#siz = null;
+		constructor(x = 0, y = 0, width = 100, height = 100, color = Color3.black()) {
+			this.#pos = new IntVector2(x, y)
+			this.#siz = new IntVector2(width, height)
+		}
+		get class() {
+			return "SquareDisplay"
+		}
+	}
+	const json = {}
+	json.version = 0;
+	json.versionID = "V1.0";
+	json.Instance = {
+		SquareDisplay
+	}
 })()
